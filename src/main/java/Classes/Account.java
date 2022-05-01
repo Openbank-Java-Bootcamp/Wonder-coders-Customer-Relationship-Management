cc5555583009c740e0fac797bfcd73cea690af6e
Paula Kamisato <paukamisato@gmail.com>
2022-05-01 12:46:09 +0200

    Account class

---
+++ src/main/java/Classes/Account.java
@@ -0,0 +1,98 @@
+package Classes;
+
+import Enums.Industry;
+
+import java.util.List;
+
+public class Account {
+
+    private int accountId;
+    private static int idCounter = 0;
+    private Industry industry;
+    private int employeeCount;
+    private String city;
+    private String country;
+    private List<Contacts> contactList;
+    private List<Opportunity> opportunityList;
+
+    public Account(Industry industry, int employeeCount, String city, String country,
+                   List<Contacts> contactList, List<Opportunity> opportunityList) {
+        idCounter++;
+        this.accountId = idCounter;
+        this.industry = industry;
+        this.employeeCount = employeeCount;
+        this.city = city;
+        this.country = country;
+        this.contactList = contactList;
+        this.opportunityList = opportunityList;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", industry=" + industry +
+                ", employeeCount=" + employeeCount +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", contactList=" + contactList +
+                ", opportunityList=" + opportunityList +
+                '}';
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public Industry getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(Industry industry) {
+        this.industry = industry;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Contacts> getContactList() {
+        return contactList;
+    }
+
+    public void setContactList(List<Contacts> contactList) {
+        this.contactList = contactList;
+    }
+
+    public List<Opportunity> getOpportunityList() {
+        return opportunityList;
+    }
+
+    public void setOpportunityList(List<Opportunity> opportunityList) {
+        this.opportunityList = opportunityList;
+    }
+}
