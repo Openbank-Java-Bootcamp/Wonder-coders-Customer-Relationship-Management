1d626f3f2d1cce176e18edd5baee7db0f8388228
beatriz-perez <beatriz.pfp@gmail.com>
2022-05-01 19:07:16 +0200

    Adds methods to print lists and select options

--- src/main/java/Classes/AppHelp.java
+++ src/main/java/Classes/AppHelp.java
@@ -40,4 +40,45 @@ public class AppHelp {
         return entry;
     }
 
+    // Ask for an integer within a range
+    public static int askForIntInRange(String question, int min, int max) {
+        boolean done = false;
+        int entry = 0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            try {
+                System.out.println(question);
+                entry = scanner.nextInt();
+                while (entry < min || entry > max) {
+                    System.err.println(entry + " is not a valid option");
+                    entry = scanner.nextInt();
+                }
+                done = true;
+            } catch (Exception e) {
+                System.err.println("You did not introduce a valid integer. Please try again.");
+                scanner.next();
+            }
+        } while (!done);
+        return entry;
+    }
+    //Select an option from a list
+    public static int selectOption(String instruction, String[] optionsList) {
+        //Show menu
+        System.out.println("\n" + instruction);
+        printList(optionsList);
+        //Ask for selected option
+        int response = askForIntInRange("\n-> Type a number from 1 to " + (optionsList.length), 1, optionsList.length);
+        //Show selected option
+        if (response > 0 && response < optionsList.length + 1) {
+            System.out.println("You have selected option " + response + ": " + optionsList[response - 1] + "\n");
+        }
+        return response - 1;
+    }
+    //Print List
+    public static void printList(String[] optionsList) {
+        for (int i = 0; i < optionsList.length; i++) {
+            System.out.println("\t\t" + (i + 1) + "." + optionsList[i]);
+        }
+    }
+
 }
